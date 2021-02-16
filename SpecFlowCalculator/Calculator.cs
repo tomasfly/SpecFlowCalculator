ed3f5eb2d3c9af3386210d19717dee6f8bfe4eb9
Tomas Fleiderman <tomas.fleiderman@newsignature.com>
2021-02-16 20:10:53 +0100

    Add project files.

---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add() => FirstNumber + SecondNumber;
+    }
+}
